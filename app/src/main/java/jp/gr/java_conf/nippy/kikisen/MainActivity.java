abcb8b6ec5d294b8a35a32e3b8cb6e3e8cda3a63
Takuma Niihara <n.tkm.dev@gmail.com>
2017-09-12 22:03:10 +0900

    now better than ts software

--- app/src/main/java/jp/gr/java_conf/nippy/kikisen/MainActivity.java
+++ app/src/main/java/jp/gr/java_conf/nippy/kikisen/MainActivity.java
@@ -1,7 +1,9 @@
 package jp.gr.java_conf.nippy.kikisen;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +17,7 @@ public class MainActivity extends Activity {
     TextView tvIP;
     EditText etSendString;
     EditText etIP;
+    TextView etLog;
     BouyomiChan4J bouyomi;
 
     /**
@@ -27,7 +30,10 @@ public class MainActivity extends Activity {
 
         tvIP = (TextView) findViewById(R.id.tvIP);
         etSendString = (EditText) findViewById(R.id.etSendString);
+        etSendString.setEnabled(true);
         etIP = (EditText) findViewById(R.id.etIP);
+        etLog = (TextView) findViewById(R.id.etLog);
+        etLog.setEnabled(false);
 
 
         //SEND button
@@ -35,19 +41,42 @@ public class MainActivity extends Activity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //ココらへんは下のkeylistenerと統合するべき TODO
                 new Thread(new Runnable() {
                     public void run() {
                         bouyomi.talk("" + etSendString.getText().toString());
                     }
                 }).start();
+                if (!(etSendString.getText().toString().equals(""))) {
+                    etLog.setText(etLog.getText().toString() + "\n" + etSendString.getText().toString());
+                }
+                etSendString.getEditableText().clear();
+            }
+        });
+        etSendString.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER) && !(etSendString.getText().toString().equals(""))) {
+                    //送信 この部分はきれいにする必要あり TODO
+                    new Thread(new Runnable() {
+                        public void run() {
+                            bouyomi.talk("" + etSendString.getText().toString());
+                        }
+                    }).start();
+                    etLog.setText(etLog.getText().toString() + "\n" + etSendString.getText().toString());
+                    etSendString.getEditableText().clear();
+                    return true;
+                }
+                return false;
             }
         });
 
+
         // SKIP button
         Button btSkip = (Button) findViewById(R.id.btSkip);
-        btSkip.setOnClickListener(new View.OnClickListener(){
+        btSkip.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 new Thread(new Runnable() {
                     public void run() {
                         bouyomi.skip();
@@ -57,10 +86,10 @@ public class MainActivity extends Activity {
         });
         //START button
         Button btStart = (Button) findViewById(R.id.btStart);
-        btStart.setOnClickListener(new View.OnClickListener(){
+        btStart.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                tvIP.setText("started on IP : " +  etIP.getText().toString());
+            public void onClick(View v) {
+                tvIP.setText("started on IP : " + etIP.getText().toString());
                 new Thread(new Runnable() {
                     public void run() {
                         bouyomi = new BouyomiChan4J(etIP.getText().toString(), 50001);
@@ -71,14 +100,14 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    public void onStart(){
+    public void onStart() {
         super.onStart();
         bouyomi = new BouyomiChan4J("192.168.11.15", 50001);
     }
 
     @Override
-    public void onStop(){
+    public void onStop() {
         super.onStop();
-        Log.v(TAG,"onStop");
+        Log.v(TAG, "onStop");
     }
 }
